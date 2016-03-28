1e9ea57aa0480853db2be85bccfa8452c2cfd0f1
Jeff Corpac <jcorpac@gmail.com>
2016-03-28 16:20:37 -0500

    Chapter 26 Complete - Added Polling Service to keep data updated - Added AlarmManager to schedule polling - Added Notification to send PendingIntent when new pictures are detected.

--- app/src/main/java/com/bignerdranch/android/photogallery/QueryPreferences.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/QueryPreferences.java
@@ -8,6 +8,7 @@ import android.preference.PreferenceManager;
  */
 public class QueryPreferences {
     public static final String PREF_SEARCH_QUERY = "searchQuery";
+    public static final String PREF_LAST_RESULT_ID = "lastResultId";
 
     public static String getStoredQuery(Context context) {
         return PreferenceManager.getDefaultSharedPreferences(context).getString(PREF_SEARCH_QUERY, null);
@@ -15,6 +16,17 @@ public class QueryPreferences {
 
     public static void setStoredQuery(Context context, String query) {
         PreferenceManager.getDefaultSharedPreferences(context).edit().putString(PREF_SEARCH_QUERY, query).apply();
+    }
+
+    public static String getPrefLastResultId(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context).getString(PREF_LAST_RESULT_ID, null);
+    }
+
+    public static void setPrefLastResultId(Context context, String lastResultId) {
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit()
+                .putString(PREF_LAST_RESULT_ID, lastResultId)
+                .apply();
     }
 
 }
